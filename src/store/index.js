2c7c9d880e9948302c64ed08d55a901c3d1640cb
Unity-JNR <unitydemas12@gmail.com>
2024-08-17 16:53:55 +0200

    reverted to old code

--- src/store/index.js
+++ src/store/index.js
@@ -14,8 +14,8 @@ export default createStore({
     ADD_MESSAGE(state, message) {
       state.messages.push(message);
     },
-    SET_MESSAGES(state, messages) {
-      state.messages = messages;
+    CLEAR_MESSAGES(state) {
+      state.messages.splice(0, state.messages.length);
     },
     SET_CURRENT_USER_ID(state, id) {
       state.currentUserId = id;
@@ -30,15 +30,9 @@ export default createStore({
           withCredentials: true,
         });
         commit('SET_SOCKET', socket);
-
         socket.on('userId', (id) => {
           commit('SET_CURRENT_USER_ID', id);
         });
-
-        socket.on('previous messages', (messages) => {
-          commit('SET_MESSAGES', messages);
-        });
-
         socket.on('chat message', (message) => {
           commit('ADD_MESSAGE', message);
         });
@@ -49,12 +43,14 @@ export default createStore({
         state.socket.emit('chat message', message);
       }
     },
-    fetchMessagesFromServer({ commit, state }) {
+    fetchMessagesFromServer({ state }) {
       if (state.socket) {
         state.socket.on('chat message', (message) => {
-          commit('ADD_MESSAGE', message);
+          state.commit('ADD_MESSAGE', message);
         });
       }
     },
   },
 });
+
+//
