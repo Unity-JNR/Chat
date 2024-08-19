d179c6e944a7c18ceaf4c05b8cc73745fc0fa0ec
Unity-JNR <unitydemas12@gmail.com>
2024-08-19 09:27:09 +0200

    added speed insights

--- src/main.js
+++ src/main.js
@@ -3,8 +3,9 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import { SpeedInsights } from "@vercel/speed-insights/vue"
 
 
-
+App.use(SpeedInsights)
 
 createApp(App).use(store).use(router).mount('#app')
